be0e5b96b9615f023800be2e7ac590a4e7486c2e
Stefaan Himpe <stefaan.himpe@curf.be>
2026-06-27 22:18:06 +0200

    feat: PCM-Drums kit editor view (header + note rail + tab shell)

---
+++ Src/Views/PCMDrumKitEditorView.axaml.cs
@@ -0,0 +1,21 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Integra7AuralAlchemist.ViewModels;
+
+namespace Integra7AuralAlchemist.Views;
+
+public partial class PCMDrumKitEditorView : UserControl
+{
+    public PCMDrumKitEditorView()
+    {
+        InitializeComponent();
+    }
+
+    // Clicking a rail row auditions that drum note (in addition to selecting it).
+    private void NoteRow_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is Control { DataContext: PCMDrumNoteViewModel note } &&
+            DataContext is PCMDrumKitEditorViewModel vm)
+            vm.PlayNote(note.Note);
+    }
+}
